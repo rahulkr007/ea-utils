b0162376a978c7addc383cc1085c742a531d12ce
rahulkr007 <rahul.kumar@medgenome.com>
2017-09-13 11:59:03 +0530

    Output file format modified

--- clipper/fastq-multx.cpp
+++ clipper/fastq-multx.cpp
@@ -1,16 +1,19 @@
 /*
 Copyright (c) 2011 Expression Analysis / Erik Aronesty
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL TH
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
@@ -18,14 +21,16 @@ THE SOFTWARE.
 */
 
 /*
+
 See "void usage" below for usage.
+
 */
 
 #include "fastq-lib.h"
 
 #define MAX_BARCODE_NUM 6000
 #define MAX_GROUP_NUM 500
-// factor to divide max b
+// factor to divide max by
 #define THFIXFACTOR 20
 #define endstr(e) (e=='e'?"end":e=='b'?"start":"n/a")
 
